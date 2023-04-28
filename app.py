c972f8f86eb87628bdda9347902337f635cd1be6
getcher123 <getcher123@gmail.com>
2023-04-28 20:34:24 +0300

    loging

--- app.py
+++ app.py
@@ -13,10 +13,15 @@ if __name__ == '__main__':
     
 @app.route('/', methods=['GET'])
 def getData():
+        print(request)
         request_data = request.get_json()
+        print(request_data)
         document_key = request_data["doc_key"]
+        print(document_key)
         sheet = request_data["sheet"]
+        print(sheet)
         creds_id_google_drive = request_data["creds_id"]
+        print(creds_id_google_drive)
         json_data = download_google_drive_json_file(creds_id_google_drive)
         result = GoogleSheetDataFrame(json_data, document_key, sheet)
         result = result.to_json(orient='records')
